98dab48f2875e0918835e91913c3e7fa78287a29
eonbre <fikomk46@gmail.com>
2019-10-01 07:44:58 +0200

    hey

--- src/containers/forms/LoginForm/index.js
+++ src/containers/forms/LoginForm/index.js
@@ -1,4 +1,4 @@
-import validators, { validateFirstName, validatePassowrd } from 'C:\\Users\\Uporabnik\\Desktop\\forma\\src\\containers\\validators.js';
+import validators, { validateFirstName, validatePassowrd } from '../../validators';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
